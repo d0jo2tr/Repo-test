e23c6c50a8d9ec73821af4555957a93d5e40ec92
dojo <g0ju2tr@gmail.com>
2021-07-26 17:18:40 +0300

    testing file...

---
+++ deneme-new.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+person1 = Person("Ali",29,"Male")
+print(person1.age)
