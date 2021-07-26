7d671e324df2cb1e0a9d9b3544703f6bcf1d5b96
dojo <g0ju2tr@gmail.com>
2021-07-26 15:09:04 +0300

    new feature in new branch

--- add_function.py
+++ add_function.py
@@ -1,8 +1,20 @@
 def add(x,y):
     return x+y
 
+def substract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
 x = int(input())
 y = int(input())
 
 result = add(x,y)
+result2 = substract(x,y)
+
 print(result)
+print(result2)
