e1f485c8f1941538bad1edde6cd3284bcffebfb7
dojo <g0ju2tr@gmail.com>
2021-07-26 17:35:41 +0300

    update op_function.py

---
+++ op_function.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+
+def substract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+x = int(input())
+y = int(input())
+
+result = add(x,y)
+result2 = substract(x,y)
+result3 = divide(x,y)
+
+print(result3)
